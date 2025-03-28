868d2b6d2d1736e461c0700ebb4468a100bcebd5
MerricsUniqueUsername <45721586+MerricsUniqueUsername@users.noreply.github.com>
2025-03-28 00:07:50 -0400

    Project links added

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -4,38 +4,38 @@
       <h1 class="text-3xl md:text-4xl font-bold mb-6">Some of my work</h1>
 
       <div class="projects flex flex-wrap items-center justify-center gap-6 md:gap-8 p-4 w-full max-w-7xl">
-        <div class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
+        <div @click="goto('https://trajectory-tuner.vercel.app/')" class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
           <img src="../assets/Images/trajtuner.png" alt="Project Image" class="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg" />
           <div class="flex mt-3 justify-between items-center">
             <span class="text-lg md:text-xl font-medium">Trajectory Tuner</span>
-            <GithubButton />
+            <GithubButton @click="goto('https://github.com/MerricsUniqueUsername/TrajectoryTuner')" />
           </div>
           <p class="text-sm md:text-base text-gray-300 mt-2">Type an expression into the input bar, and click the launch button. Try to get the trajectory of the expression to collect all the yellow coins and avoid the red coins. If fuel runs out, the player will continue in the direction it was going when fuel ran out.</p>
         </div>
 
-        <div class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
+        <div @click="goto('https://click-game-opal.vercel.app/')" class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
           <img src="../assets/Images/clickgame.png" alt="Project Image" class="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg" />
           <div class="flex mt-3 justify-between items-center">
             <span class="text-lg md:text-xl font-medium">Click Game</span>
-            <GithubButton />
+            <GithubButton @click="goto('https://github.com/MerricsUniqueUsername/ClickGame')"/>
           </div>
           <p class="text-sm md:text-base text-gray-300 mt-2">Click the button a given amount of times in the time limit given. Try to get the number of clicks to zero before time runs out without going negative. This game records your click speed and ranks you based on how far you get.</p>
         </div>
 
-        <div class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
+        <div @click="goto('https://crossword-builder-five.vercel.app/editor.html')" class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
           <img src="../assets/Images/crossword.png" alt="Project Image" class="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg" />
           <div class="flex mt-3 justify-between items-center">
             <span class="text-lg md:text-xl font-medium">Crossword Builder</span>
-            <GithubButton />
+            <GithubButton @click="goto('https://github.com/MerricsUniqueUsername/CrosswordBuilder')" />
           </div>
           <p class="text-sm md:text-base text-gray-300 mt-2">Build a crossword puzzle. Click a square to edit it, and type words into the crossword. Press the "." key to add a black square that shows symmetrically. Use arrow keys to move around and switch direction. This records the average length of your word and puts all the words you put in a list.</p>
         </div>
 
-        <div class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
+        <div @click="goto('https://iq-test-eight.vercel.app/')" class="project fade w-full sm:w-[calc(50%-20px)] md:w-[calc(33%-22px)] lg:w-[calc(25%-24px)] xl:w-[340px]">
           <img src="../assets/Images/test.png" alt="Project Image" class="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg" />
           <div class="flex mt-3 justify-between items-center">
             <span class="text-lg md:text-xl font-medium">Frustrating Test</span>
-            <GithubButton />
+            <GithubButton @click="goto('https://github.com/MerricsUniqueUsername/IQTest')"/>
           </div>
           <p class="text-sm md:text-base text-gray-300 mt-2">An intentionally extremely frustrating philosophy test that tells you your IQ at the end. It attempts to make you look really bad and confused, while strawmanning your position. The whole thing is generated with AI.</p>
         </div>
@@ -72,6 +72,11 @@ export default {
     };
 
     onMounted(fadeInProjects);
+  },
+  methods: {
+    goto(url) {
+      window.open(url, '_blank');
+    }
   }
 }
 </script>
